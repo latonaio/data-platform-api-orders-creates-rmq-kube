a7a646740218a91cd21582eacc6df1a43036d9d5
MiyuYokoyama0109 <miyu.y@latona.io>
2022-11-10 19:49:18 +0900

    release

--- existence_check/queueMapper.go
+++ existence_check/queueMapper.go
@@ -1,8 +1,8 @@
 package existence_check
 
 var m = map[string]string{
-	"Product":         "data-platform-api-product-master-exconf-queue-from",
-	"BusinessPartner": "data-platform-api-business-partner-exconf-queue-from",
+	"Product":         "data-platform-api-product-master-exconf-queue",
+	"BusinessPartner": "data-platform-api-business-partner-exconf-queue",
 }
 
 func getQueueNameByCheckContent(content string) string {
