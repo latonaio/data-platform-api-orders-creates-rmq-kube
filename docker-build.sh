a0dc8d49b637afe650e081d1972a07c62f86719e
kimura <creative@latona.io>
2023-05-23 18:10:46 +0900

    release

--- docker-build.sh
+++ docker-build.sh
@@ -5,7 +5,7 @@ DATE="$(date "+%Y%m%d%H%M")"
 REPOSITORY_PREFIX="latonaio"
 SERVICE_NAME="data-platform-api-orders-creates-rmq-kube"
 
-DOCKER_BUILDKIT=1 docker build --progress=plain -t ${SERVICE_NAME}:"${DATE}" . --no-cache
+DOCKER_BUILDKIT=1 docker build --platform linux/amd64 --progress=plain -t ${SERVICE_NAME}:"${DATE}" . --no-cache 
 
 # tagging
 docker tag ${SERVICE_NAME}:"${DATE}" ${SERVICE_NAME}:latest
